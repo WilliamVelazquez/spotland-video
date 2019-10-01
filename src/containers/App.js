93b8d9ae746e51ba4014a1c4a1d4880be50aea26
William Velazquez <william.velazquez.a@gmail.com>
2019-10-01 18:41:18 -0500

    Creating Categories, Carousel and CarouselItem Components and Styles, Adding Icon Assets and Handling API data

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Footer from '../components/Footer';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
 import EllipsisLoader from '../components/EllipsisLoader';
 
 import useInitialState from '../hooks/useInitialState';
@@ -12,10 +15,57 @@ const API = 'http://localhost:3000/initialState/';
 
 const App = () => {
   const initialState = useInitialState(API);
+  // console.log(initialState);
   return initialState.length === 0 ? <div style={{ display: 'flex', justifyContent: 'center' }}><EllipsisLoader /></div> : (
     <>
       <Header />
       <Search />
+
+      {
+        initialState.myList.length > 0 &&
+        (
+          <Categories title='My list'>
+            <Carousel>
+              {
+                initialState.myList.map(item => (
+                  <CarouselItem key={item.id} {...item} />
+                ))
+              }
+            </Carousel>
+          </Categories>
+        )
+      }
+
+      {
+        initialState.trends.length > 0 &&
+        (
+          <Categories title='Trends'>
+            <Carousel>
+              {
+                initialState.trends.map(item => (
+                  <CarouselItem key={item.id} {...item} />
+                ))
+              }
+            </Carousel>
+          </Categories>
+        )
+      }
+
+      {
+        initialState.originals.length > 0 &&
+        (
+          <Categories title='Spotland Originals'>
+            <Carousel>
+              {
+                initialState.originals.map(item => (
+                  <CarouselItem key={item.id} {...item} />
+                ))
+              }
+            </Carousel>
+          </Categories>
+        )
+      }
+
       <Footer />
     </>
   );
