254a364a1eed7838feb174a9921b38aba97def3b
William Velazquez <william.velazquez.a@gmail.com>
2019-09-30 20:08:23 -0500

    Updating Webpack settings to support svg files with the File Loader, Updating Header Component and Adding Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
-const Header = () => <h1>App</h1>;
+import logo from '../assets/static/logo.svg';
+import userIcon from '../assets/static/user-icon.png';
+import '../assets/styles/components/Header.scss';
+
+const Header = () => (
+  <header className='header'>
+    <div className='header__logo_container'>
+      <img className='header__img' src={logo} alt='Spotland Video' />
+      <span className='header__title'>Spotland</span>
+    </div>
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src={userIcon} alt='User Icon' />
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li><a href='/'>Cuenta</a></li>
+        <li><a href='/'>Cerrar Sesión</a></li>
+      </ul>
+    </div>
+  </header>
+);
 
 export default Header;
